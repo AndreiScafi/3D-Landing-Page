cc45961535146f2382dc7507c4cb66f9f50c5c88
Andrei Scafi <andreiscafi@hotmail.com>
2023-08-29 09:53:20 -0300

    Changing the span text and color with animation.

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 const boxes = document.querySelectorAll(".box");
+const heading = document.querySelector(".heading span");
+const btn = document.querySelector(".btn");
+
+const colors = [
+    "#efd81d",
+    "#61dbfb",
+    "#41b883",
+    "#b52e31",
+    "#43853d",
+    "#cf649a",
+    "#e04e39"
+]
+
+const techs = [
+    "JS",
+    "React",
+    "Vue",
+    "Angular",
+    "Node",
+    "Sass",
+    "Ember"
+]
 
 let current = 1;
+
+const textStyle = () => {
+    heading.style.color = colors[current - 1];
+    heading.textContent = techs[current - 1];
+    btn.style.backgroundColor = colors[current - 1];
+    btn.firstElementChild.textContent = techs[current - 1];
+}
 
 let interval = setInterval(() => {
     boxes.forEach((box) => {
@@ -12,6 +41,7 @@ let interval = setInterval(() => {
             box.classList.remove("active");
         }
     })
+    textStyle()
     current++;
 }, 5000);
 
